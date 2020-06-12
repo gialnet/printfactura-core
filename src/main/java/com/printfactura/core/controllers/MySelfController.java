bd6f7d590d78f3a1ed506151b4538d8c50cd7033
Antonio Perez Caballero <antonio.gialnet@gmail.com>
2020-06-12 21:28:53 +0200

    myself.html form

--- src/main/java/com/printfactura/core/controllers/MySelfController.java
+++ src/main/java/com/printfactura/core/controllers/MySelfController.java
@@ -1,6 +1,8 @@
 package com.printfactura.core.controllers;
 
 import com.printfactura.core.domain.MySelf;
+import com.printfactura.core.domain.appusers.AppUser;
+import com.printfactura.core.services.rocksdb.ServicesUsers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,10 +12,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Controller
 public class MySelfController {
+
+    private final ServicesUsers servicesUsers;
+
+    public MySelfController(ServicesUsers servicesUsers) {
+        this.servicesUsers = servicesUsers;
+    }
 
     @GetMapping("/register")
     public String showForm(Model model) {
@@ -27,6 +36,15 @@ public class MySelfController {
         return "register_form";
     }
 
+    @GetMapping("/myself")
+    public String showFormMySelf(Model model) {
+
+        MySelf mySelf = new MySelf();
+        model.addAttribute("MySelf", mySelf);
+
+        return "myself";
+    }
+
     @PostMapping("/register")
     public String submitForm(@ModelAttribute("mySelf") MySelf mySelf) {
         System.out.println(mySelf);
@@ -37,12 +55,21 @@ public class MySelfController {
     @GetMapping("/login")
     public String showFormLogin(Model model) {
 
-        /*MySelf mySelf = new MySelf();
-        model.addAttribute("mySelf", mySelf);
-
-        List<String> listProfession = Arrays.asList("Developer", "Tester", "Architect");
-        model.addAttribute("listProfession", listProfession);*/
+        var appUser = AppUser.builder().build();
+        model.addAttribute("AppUser",appUser);
 
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String submitFormUser(@ModelAttribute("AppUser") AppUser appUser) {
+        System.out.println(appUser);
+        log.info("saving value '{}'", appUser);
+
+        Optional result = servicesUsers.FindUser(appUser.getIdUser());
+        if (result.isEmpty())
+            return "AlertMessage";
+
+        return "menu";
     }
 }
