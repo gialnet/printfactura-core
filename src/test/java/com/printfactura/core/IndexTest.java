47333004cbe8475aa33355663714a66738dca03c
Antonio Perez Caballero <antonio.gialnet@gmail.com>
2020-07-06 01:57:47 +0200

    more index test for update records and delete lucene records

--- src/test/java/com/printfactura/core/IndexTest.java
+++ src/test/java/com/printfactura/core/IndexTest.java
@@ -2,31 +2,39 @@ package com.printfactura.core;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.*;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.*;
 import org.apache.lucene.index.memory.MemoryIndex;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+//@SpringBootTest
 public class IndexTest {
+
+    private static IndexWriter writer;
+
+    @BeforeAll
+    public static void SetUp() throws IOException {
+        FSDirectory dir = FSDirectory.open(Paths.get("/tmp"));
+        IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
+        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+
+        // Create index in Memory RAM
+        //MemoryIndex writer = new MemoryIndex();
+        writer = new IndexWriter(dir, config);
+    }
 
     @Test
     public void OpenSearcher() throws IOException {
 
-        FSDirectory dir = FSDirectory.open(Paths.get("/tmp"));
-        IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
-        // Create index in Memory RAM
-        //MemoryIndex writer = new MemoryIndex();
-        IndexWriter writer = new IndexWriter(dir, config);
 
         Document document = new Document();
 
@@ -45,9 +53,12 @@ public class IndexTest {
         writer.addDocument(document);
 
         document =  MakeDoc(LocalDate.parse("2020-07-01").toEpochDay());
-        writer.addDocument(document);
-        writer.commit();
-        writer.close();
+
+
+       long iddoc = writer.addDocument(document);
+
+       writer.commit();
+       writer.close();
     }
 
     public Document MakeDoc(Long point){
@@ -95,5 +106,36 @@ public class IndexTest {
 
         System.out.println(LocalDate.now().minusMonths(1).toEpochDay());
         System.out.println(LocalDate.now().toEpochDay());
+    }
+
+    @Test
+    public void UpdateDoc() throws IOException {
+
+        Document doc = MakeDoc(LocalDate.parse("2020-05-14").toEpochDay());
+
+        // updateDocument only work with Text fields or StringField
+        // for numeric values delete first and the insert
+        long result = writer.updateDocument(new Term("DateInvoice",
+                String.valueOf(LocalDate.parse("2020-05-12").toEpochDay())), doc );
+        System.out.println(result);
+        long nd=writer.commit();
+
+        writer.close();
+        System.out.println(nd);
+    }
+
+    @Test
+    public void DeleteDoc() throws IOException {
+
+        long result = writer.deleteDocuments(
+                LongPoint.newExactQuery("DateInvoice",
+                        LocalDate.parse("2020-05-14").toEpochDay()) );
+
+        System.out.println(result);
+        long nd=writer.commit();
+
+        writer.close();
+        System.out.println(nd);
+
     }
 }
